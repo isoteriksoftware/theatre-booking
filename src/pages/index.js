76dabc208eec19187138d902954d6173a35484f5
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-04-10 18:42:23 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Button, makeStyles, Typography } from "@material-ui/core";
+import { Button, Grid, makeStyles, Typography } from "@material-ui/core";
 import { useEffect } from "react";
 import Header from "../components/Header";
 import { scrollToTop } from "../components/utils";
@@ -30,7 +30,47 @@ const useStyles = makeStyles(theme => ({
     textTransform: 'capitalize',
     fontSize: '1.5rem',
     borderRadius: '4rem'
-  }
+  },
+  showsContainer: {
+    padding: '2rem 5rem',
+    marginTop: '2rem',
+  },
+  showsHeader: {
+    fontWeight: 'bold',
+  },
+  showContainer: {
+    padding: '2rem 5rem',
+    marginTop: '2rem',
+  },
+  showCard: {
+    background: 'white',
+    boxShadow: '0px 2px 20px rgba(0, 0, 0, 0.05)',
+    borderRadius: '2rem',
+    padding: '1rem'
+  },
+  showImg: {
+    width: '100%',
+    height: 'auto',
+    borderRadius: '1rem',
+  },
+  showTitle: {
+    fontWeight: 'bold',
+    marginBottom: '1rem',
+    color: theme.palette.secondary.main,
+  },
+  showContent: {
+    lineHeight: '30px',
+    fontSize: '.9rem',
+  },
+  showDate: {
+    textAlign: "right",
+    paddingTop: '.5rem',
+  },
+  bookShowBtn: {
+    marginTop: '1rem',
+    textTransform: 'capitalize',
+    borderRadius: '4rem'
+  },
 }));
 
 const Index = () => {
@@ -39,6 +79,32 @@ const Index = () => {
   useEffect(() => {
     scrollToTop();
   }, []);
+
+  const ShowCard = ({ show = { image: "images/card 10.jpg", name: "The Avengers",
+    description: "" } }) => {
+    return (
+      <Grid item xs={12} md={6}>
+        <div className={classes.showCard}>
+          <Grid container spacing={3}>
+            <Grid item xs={4}><img src={show.image} alt="Cover" className={classes.showImg}/></Grid>
+            <Grid item xs={8}>
+              <Typography variant="h6" className={classes.showTitle}>{show.name}</Typography>
+              <Typography variant="body1" className={classes.showContent}>
+                "We got a guy with things comin’ out of his hands, we got another guy who freezes stuff, 
+                and then there’s a man, who as far as I can tell, is made out of electricity. I mean, how did he disappear like that? What is goin’ on here? 
+                WHO IS THIS GUY?"
+                {show.description}
+              </Typography>
+              <div className={classes.showDate}>
+                <Typography variant="caption" color="textSecondary">2020-01-05 12:12:00</Typography>
+              </div>
+              <Button fullWidth variant="outlined" color="secondary" className={classes.bookShowBtn}>Book A Seat</Button>
+            </Grid>
+          </Grid>
+        </div>
+      </Grid>
+    );
+  };
 
   return (
     <div>
@@ -55,6 +121,16 @@ const Index = () => {
             </Typography>
             <Button fullWidth variant="contained" color="secondary" className={classes.showCaseBtn}>Create Account</Button>
           </div>
+        </div>
+      </div>
+
+      <div className={classes.showsContainer}>
+        <Typography variant="h5" className={classes.showsHeader}>Scheduled Performances</Typography>
+
+        <div className={classes.showContainer}>
+          <Grid container alignItems="center" spacing={3}>
+            <ShowCard/> <ShowCard/> <ShowCard/> <ShowCard/>
+          </Grid>
         </div>
       </div>
     </div>
