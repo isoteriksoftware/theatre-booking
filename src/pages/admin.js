6c04256f0539cfd5d9d6de5f321f3c349f3ee97d
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-04-11 12:56:57 +0100

    update

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,10 +1,20 @@
 import { Button, Dialog, DialogContent, DialogTitle, Grid, makeStyles, Typography } from "@material-ui/core";
 import { Form, Formik } from "formik";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import SlideTransition from "../components/SlideTransition";
 import * as yup from "yup";
 import FormikField from "../components/FormikField";
+import { connect } from "react-redux";
+import { useHistory } from "react-router";
+import { axiosInstance, generateErrorsMarkup, showError, showInfo, showLoading, showNetworkError, showSuccess } from "../components/utils";
+import Skeleton from '@material-ui/lab/Skeleton';
+import MomentUtils from '@date-io/moment';
+import moment from 'moment';
+import {
+    KeyboardDatePicker ,
+    MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -68,6 +78,7 @@ const useStyles = makeStyles(theme => ({
     '& .field': {
       marginBottom: '1.2rem',
       borderRadius: '5px',
+      width: '100%',
     },
     '& .field .inner': {
       padding: '.2rem'
@@ -87,33 +98,111 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const Admin = () => {
+const Admin = connect(state => ({
+  auth: {...state.userReducer.auth},
+}))(({ auth }) => {
   const classes = useStyles();
   const [showDialog, setShowDialog] = useState(false);
+  const [shows, setShows] = useState(null);
+  const [imageFile, setImageFile] = useState(null);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-  const ShowCard = ({ show = { image: "images/card 10.jpg", name: "The Avengers",
-    description: "" } }) => {
+  const history = useHistory();
+
+  const ShowCard = ({ show }) => {
     return (
       <Grid item xs={12} md={6}>
         <div className={classes.showCard}>
           <Grid container spacing={3}>
-            <Grid item xs={4}><img src={show.image} alt="Cover" className={classes.showImg}/></Grid>
+            <Grid item xs={4}><img src={show.image_url} alt="Cover" className={classes.showImg}/></Grid>
             <Grid item xs={8}>
               <Typography variant="h6" className={classes.showTitle}>{show.name}</Typography>
               <Typography variant="body1" className={classes.showContent}>
-                "We got a guy with things comin’ out of his hands, we got another guy who freezes stuff, 
-                and then there’s a man, who as far as I can tell, is made out of electricity. I mean, how did he disappear like that? What is goin’ on here? 
-                WHO IS THIS GUY?"
                 {show.description}
               </Typography>
               <div className={classes.showDate}>
-                <Typography variant="caption" color="textSecondary">2020-01-05 12:12:00</Typography>
+                <Typography variant="caption" color="textSecondary">{show.start_date}</Typography>
               </div>
             </Grid>
           </Grid>
         </div>
       </Grid>
     );
+  };
+
+  useEffect(() => {
+    if (!auth.authenticated || !auth.isAdmin)
+      history.push('/logout');
+  }, [auth, history]);
+
+  useEffect(() => {
+    axiosInstance.get('admin/show')
+    .then(res => {
+      if (res.status === 200) {
+        const data = res.data;
+        setShows(data);
+      }
+      else if (res.status === 404)
+        showInfo('Oops!', 'No scheduled performances at this time.');
+      else if (res.status === 401)
+        history.push('/logout');
+    })
+    .catch(() => {});
+  }, [history]);
+
+  const reloadShows = () => {
+    setShows(null);
+
+    axiosInstance.get('admin/show')
+    .then(res => {
+      if (res.status === 200) {
+        const data = res.data;
+        setShows(data);
+      }
+      else if (res.status === 404)
+        showInfo('Oops!', 'No scheduled performances at this time.');
+      else if (res.status === 401)
+        history.push('/logout');
+    })
+    .catch(() => {});
+  };
+
+  const handleFileChanged = e => {
+    setImageFile(e.target.files[0]);
+  };
+
+  const doAddShow = (values) => {
+    showLoading();
+
+    const formData = new FormData();
+    formData.append('description', values.description);
+    formData.append('name', values.name);
+    formData.append('start_date', moment(startDate).format('yyyy-MM-DD'));
+    formData.append('end_date', moment(endDate).format('yyyy-MM-DD'));
+    formData.append('image', imageFile);
+
+    axiosInstance.post('admin/show', formData, {
+      headers: {
+          'content-type': 'multipart/form-data'
+      }
+    })
+    .then(res => {
+      if (res.status === 201) {
+        showSuccess('Show Added!');
+        reloadShows();
+      }
+      else if (res.status === 400) {
+        console.log(res.data);
+        const body = generateErrorsMarkup(res.data.messages.error);
+        showError('Oops!', body);
+      }
+      else if (res.status === 401)
+        history.push('/logout');
+      else
+        showNetworkError();
+    })
+    .catch(() => showNetworkError());
   };
 
   return (
@@ -127,7 +216,10 @@ const Admin = () => {
 
         <div className={classes.showContainer}>
           <Grid container alignItems="center" spacing={3}>
-            <ShowCard/> <ShowCard/> <ShowCard/> <ShowCard/>
+            {shows ? (
+              shows.map((show, index) => <ShowCard key={index} show={show}/>)
+            ) :
+            <Skeleton variant="rect" width="100%" height={300}/>}
           </Grid>
         </div>
       </div>
@@ -135,6 +227,7 @@ const Admin = () => {
       <Dialog
         fullWidth
         maxWidth="sm"
+        scroll="body"
         open={showDialog}
         onClose={() => setShowDialog(false)}
         TransitionComponent={SlideTransition}
@@ -143,60 +236,94 @@ const Admin = () => {
           <DialogContent>
             <Formik
               initialValues={{
-                username: '',
+                description: '',
                 name: '',
-                password: '',
               }}
 
               validationSchema={yup.object({
-                username: yup.string()
-                  .max(12, 'Username should not be greater than 12 characters')
-                  .required('Please enter your username'),
+                description: yup.string()
+                  .max(1000, 'Description should not be greater than 1000 characters')
+                  .required('Please enter the description'),
                 name: yup.string()
-                  .max(12, 'Name should not be greater than 30 characters')
+                  .max(30, 'Name should not be greater than 30 characters')
                   .required('Please enter your name'),
-                password: yup.string()
-                  .required('Please enter your password'),
               })}
 
-              //onSubmit={doLogin}
+              onSubmit={doAddShow}
               >
-              <Form className={classes.form}>
-                <FormikField
-                  name="name"
-                  variant="outlined"
-                  label="Name"
-                  color="primary"
-                  className="field"
-                  InputProps={{ className: "inner" }}
-                />
-                <FormikField
-                  name="description"
-                  variant="outlined"
-                  label="Description"
-                  color="primary"
-                  className="field"
-                  multiline
-                  rows={6}
-                  InputProps={{ className: "inner" }}
-                />
-                
-                <Button
-                  fullWidth
-                  type="submit" 
-                  variant="contained" 
-                  color="primary" 
-                  size="large"
-                  className="submit-btn"
-                  >
-                    Register
-                  </Button>
-              </Form>
+                <MuiPickersUtilsProvider utils={MomentUtils}>
+                  <Form className={classes.form}>
+                    <FormikField
+                      name="name"
+                      variant="outlined"
+                      label="Name"
+                      color="primary"
+                      className="field"
+                      InputProps={{ className: "inner" }}
+                    />
+                    <FormikField
+                      name="description"
+                      variant="outlined"
+                      label="Description"
+                      color="primary"
+                      className="field"
+                      multiline
+                      rows={6}
+                      InputProps={{ className: "inner" }}
+                    />
+                    <KeyboardDatePicker
+                      margin="normal"
+                      label="Start Date"
+                      format="yyyy-MM-DD"
+                      value={startDate}
+                      onChange={date => setStartDate(date)}
+                      KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                      }}
+                      className="field"
+                    />
+                    <KeyboardDatePicker
+                      margin="normal"
+                      label="End Date"
+                      format="yyyy-MM-DD"
+                      value={endDate}
+                      onChange={date => setEndDate(date)}
+                      KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                      }}
+                      className="field"
+                    />
+                    <Typography variant="caption" color="textSecondary">Choose Cover Image:</Typography>
+                    <FormikField
+                      color="primary"
+                      name="file"
+                      label=""
+                      type="file"
+                      required
+                      variant="outlined"
+                      className="field"
+                      fullWidth
+                      inputProps={{accept: 'image/*'}}
+                      onChange={handleFileChanged}
+                    />
+                    
+                    <Button
+                      fullWidth
+                      type="submit" 
+                      variant="contained" 
+                      color="primary" 
+                      size="large"
+                      className="submit-btn"
+                      >
+                        Add Show
+                      </Button>
+                  </Form>
+                </MuiPickersUtilsProvider>
             </Formik>
           </DialogContent>
         </Dialog>
     </div>
   );
-};
+});
 
 export default Admin;
